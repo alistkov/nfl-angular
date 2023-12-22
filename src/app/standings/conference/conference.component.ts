f7f83f9c219dbd0a900b6c3e26d097e75b7155b9
Anton Listkov <antonlistkov@gmail.com>
2023-12-23 00:59:11 +0100

    feat: add conference component

---
+++ src/app/standings/conference/conference.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfl-conference',
+  templateUrl: './conference.component.html',
+  styleUrls: ['./conference.component.css']
+})
+export class ConferenceComponent {
+
+}
