7f4ee8bfb5c4ae28c6d5cea0353a1011337cfde6
Anton Listkov <antonlistkov@gmail.com>
2023-12-23 00:38:46 +0100

    feat: add standings module

---
+++ src/app/standings/standings.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StandingsRoutingModule } from './standings-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StandingsRoutingModule,
+  ],
+  declarations: [
+  ],
+})
+export class StandingsModule {}
