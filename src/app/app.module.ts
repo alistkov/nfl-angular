90fd749c882a3a3ca2518f35c65d9f6f7c53b683
Anton Listkov <antonlistkov@gmail.com>
2023-12-22 23:18:17 +0100

    feat: add games and teams components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TeamsComponent } from './teams/teams.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TeamsComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
