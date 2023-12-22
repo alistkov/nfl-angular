35d673b0875c97310a83a253e90606214c760d88
Anton Listkov <antonlistkov@gmail.com>
2023-12-22 12:19:17 +0100

    init: init rpoject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'nfl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
