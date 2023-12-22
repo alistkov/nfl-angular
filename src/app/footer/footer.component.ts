2cb9d53c87aa5e2e1b3071c1cbff2928646d4b5c
Anton Listkov <antonlistkov@gmail.com>
2023-12-22 23:05:39 +0100

    feat: add footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfl-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  logo: string = 'assets/images/logo.svg';
+}
