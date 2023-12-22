be6eda982b0ad1b6404bb5805c68544915916066
Anton Listkov <antonlistkov@gmail.com>
2023-12-22 23:35:33 +0100

    feat: add base navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TeamsComponent } from './teams/teams.component';
+import { GamesComponent } from './games/games.component';
+
+const routes: Routes = [
+  { path: 'teams', component: TeamsComponent, title: 'NFL App | Teams' },
+  { path: 'games', component: GamesComponent, title: 'NFL App | Games' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
