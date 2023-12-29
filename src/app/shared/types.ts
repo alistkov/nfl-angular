72dd75d539995e68c198b049759a6e28bed6fee2
Anton Listkov <antonlistkov@gmail.com>
2023-12-29 12:49:41 +0100

    feat: add team and division standings types

--- src/app/shared/types.ts
+++ src/app/shared/types.ts
@@ -2,3 +2,60 @@ export interface Link {
   link: string
   label: string
 }
+
+export interface Team {
+  "league": {
+    "id": number
+    "name": string
+    "season": number
+    "logo": string
+    "country": {
+      "name": string
+      "code": string
+      "flag": string
+    }
+  }
+  "conference": "American Football Conference",
+  "division": string
+  "position": number
+  "team": {
+    "id": number
+    "name": string
+    "logo": string
+  }
+  "won": number
+  "lost": number
+  "ties": number
+  "points": {
+    "for": number
+    "against": number
+    "difference": number
+  }
+  "records": {
+    "home": string
+    "road": string
+    "conference": string
+    "division": string
+  }
+  "streak": string
+  "ncaa_conference": {
+    "won": null
+    "lost": null
+    "points": {
+      "for": null
+      "against": null
+    }
+  }
+}
+
+export interface DivisionStandings {
+  "get": string
+  "parameters": {
+    "league": string | number
+    "season": string | number
+    "division": string
+  },
+  "errors": [],
+  "results": number
+  "response": Team[]
+}
