698a7e2c85121d7b29e0d8c73a86ea5734616df8
Anton Listkov <antonlistkov@gmail.com>
2024-01-11 00:33:24 +0100

    refactor: replace footer with standalone

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [RouterLink],
   selector: 'nfl-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
   logo: string = 'assets/images/logo.svg';
