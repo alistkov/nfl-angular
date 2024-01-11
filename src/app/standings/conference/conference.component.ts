3165b1d602619958d22a37f60049281bb9169968
Anton Listkov <antonlistkov@gmail.com>
2024-01-11 02:28:09 +0100

    feat: show warning alert

--- src/app/standings/conference/conference.component.ts
+++ src/app/standings/conference/conference.component.ts
@@ -2,11 +2,16 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { CONFERENCES } from 'src/app/shared/constants';
+import { ErrorAlertComponent } from 'src/app/shared/error-alert/error-alert.component';
 import { StandingsTableComponent } from 'src/app/standings/standings-table/standings-table.component';
 
 @Component({
   standalone: true,
-  imports: [NgFor, StandingsTableComponent],
+  imports: [
+    NgFor,
+    StandingsTableComponent,
+    ErrorAlertComponent,
+  ],
   selector: 'nfl-conference',
   templateUrl: './conference.component.html',
 })
